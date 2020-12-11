c8e9961e6720916e02becabf812eb9868d8b7b5f
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-11 16:39:05 -0400

    Diferencia entre function expression y declaration

--- 09-Funciones/js/02-app.js
+++ 09-Funciones/js/02-app.js
@@ -1,15 +1,16 @@
+sumar();
+
 // Declaración de función (Function declaration)
 function sumar() {
     console.log(2 + 2);
 }
 
-sumar();
-sumar();
-sumar();
 
 // Expresión de Función (Function Expression)
+sumar2();
 const sumar2 = function() {
     console.log(3 + 3)
 }
 
-sumar2();
+
+/* La diferencia entre expresión de función y declaración de funcione s que la declarada la podemos utilizar incluso antes y la de expresion no. A esto es lo que se le denomina ---hoisting--- */
