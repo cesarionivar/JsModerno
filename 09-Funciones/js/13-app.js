52d357f58d982c89ed3c02ae151169b6704eeb7f
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-13 19:44:50 -0400

    set y get objetos y arrow functions

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -1,9 +1,26 @@
 const reproductor = {
+    cancion: '',
     reproducir:  id => console.log(`Reproduciendo canción con el id ${id}`),
     pausar: () => console.log(`Pausando...`),
     borrar: id => console.log(`Borrando canción... ${id}`),
     playlist: id => console.log(`Agregando canción ${id}`),
-    reproducirPlaylist: nombre => console.log(`Reproduciendo la playlist ${nombre}`) 
+    reproducirPlaylist: nombre => console.log(`Reproduciendo la playlist ${nombre}`),
+
+    set nuevaCancion(cancion) {
+        this.cancion = cancion;
+        console.log(`Añadiendo ${cancion}`);
+    },
+
+    get obtenerCancion() {
+        console.log(`${this.cancion}`);
+    }
 }
 
+
+reproductor.nuevaCancion = 'Enter Sadman';
+reproductor.obtenerCancion;
+
 reproductor.reproducir(10);
+reproductor.pausar();
+reproductor.borrar(45);
+reproductor.reproducirPlaylist('Dembow');
