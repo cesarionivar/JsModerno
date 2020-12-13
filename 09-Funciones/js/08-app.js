c15365f5309145cd92b5caa3ddc40a8906ca2b02
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-13 19:11:45 -0400

    Ejemplo con funciones mas avanzado

--- 09-Funciones/js/08-app.js
+++ 09-Funciones/js/08-app.js
@@ -5,3 +5,25 @@ function sumar(a, b) {
 const resultado = sumar(2, 3);
 
 console.log(resultado);
+
+// Ejemplo más avanzado
+
+let total = 0;
+function agregarCarrito(precio) {
+    return total += precio;
+}
+
+function calcularImpuesto(total) {
+    return total * 1.18;
+}
+
+agregarCarrito(300);
+agregarCarrito(100);
+agregarCarrito(600);
+
+
+const totalPagar = calcularImpuesto(total);
+
+console.log(`El total a pagar es ${totalPagar}`);
+
+console.log(total);
