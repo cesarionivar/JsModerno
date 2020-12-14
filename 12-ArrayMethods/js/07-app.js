1fa20f6db36a559e581d9e854a29f3712eca85b6
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-14 14:41:42 -0400

    Concatenando arrays .concat and rest o spread operator

---
+++ 12-ArrayMethods/js/07-app.js
@@ -0,0 +1,18 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const meses2 = [
+    'Agosto', 'Septiembre'
+];
+
+const meses3 = [
+    'Octubre', 'Noviembre', 'Diciembre'
+];
+
+// .concat
+const resultado = meses.concat(meses2);
+console.log(resultado);
+
+
+// Spread operator or rest operator
+const resultado2 = [...meses, ...meses2, ...meses3];
+console.log(resultado2);
