90a894be18bf2eb24e05301cc847a4095354fb12
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-14 14:03:25 -0400

    Comprobar si un valor existe en un array (.some)

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -7,4 +7,24 @@ const carrito = [
     { nombre: 'Audifonos', precio: 300 },
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
-]
+];
+
+// Comprobar si un valor existe en un arreglo
+
+meses.forEach(mes => {
+    if(mes == 'Enero') {
+        console.log(`Enero si existe`);
+    }
+});
+ 
+// Verificar si incluye un valor en un arreglo
+const resultado = meses.includes('Enero');
+console.log(resultado);
+
+// Verificar si incluye un valor en un arreglo de objetos (.some)
+const existe = carrito.some( producto => producto.nombre === 'Monitor');
+console.log(existe);
+
+// En un arreglo tradicional con .some
+const existe2 = meses.some(mes => mes === 'Febrero');
+console.log(existe2);
