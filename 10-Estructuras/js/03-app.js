c228a8b11efaf10557f58925ae348075e0899a03
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-13 22:04:46 -0400

    Comparando mayor que y menor que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,10 @@
+// Operador mayor que y menor que
+
+const dinero = 100;
+const totalAPagar = 300;
+
+if(dinero >= totalAPagar) {
+    console.log('Si puedes pagar');
+} else {
+    console.log('Fondos insuficientes');
+}
