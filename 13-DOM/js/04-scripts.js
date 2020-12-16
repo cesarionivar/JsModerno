af22b9534cf35031db87cd74cebaec3db0cff3d1
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-16 08:32:26 -0400

    querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,24 @@
+// querySelector
+
+const card = document.querySelector('.card');
+console.log(card);
+
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+
+// Podemos tener selectores especificos como en CSS
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+
+// Seleccionar el segundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+// Seleccionar el formulario
+const formulario = document.querySelector('.contenido-hero #formulario');
+console.log(formulario);
+
+// Selecciona elementos HTML
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
