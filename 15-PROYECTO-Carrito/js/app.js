d5453dd7e63bf24fcfde297ad16d6fccd1f024c4
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-17 16:46:42 -0400

    Variables proyecto carrito

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,6 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
