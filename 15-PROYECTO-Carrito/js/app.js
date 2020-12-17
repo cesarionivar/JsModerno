4ee6463831ae801cdb431d0085f38971ed45c216
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-17 16:52:33 -0400

    Función agregar curso al carrito

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -4,3 +4,16 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+cargarEventListener();
+function cargarEventListener() {
+    // Cuando agregas un curso presionando "Agregar al Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        console.log('Agregando al carrito...');
+    }
+}
