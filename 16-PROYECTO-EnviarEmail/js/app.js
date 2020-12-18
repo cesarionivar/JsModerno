03fa2fb97ba66edc00f7132806dfdf45222601b8
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-18 17:50:45 -0400

    Limpiar los mensajes de error

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,10 +1,11 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 // Variables para campos
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
-const mensaje = document.querySelector('#mesanje');
+const mensaje = document.querySelector('#mensaje');
 
 
 eventListeners();
@@ -29,9 +30,25 @@ function validarFormulario(e) {
     
     if(e.target.value.length > 0) {
         console.log('Si hay algo');
+
     } else {
-        e.target.style.borderBottomColor = 'red';
+
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
+    }
+
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+
+    if(errores.length === 0) {
+        formulario.appendChild(mensajeError);
     }
 
 }
