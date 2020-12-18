5bfdf56522dd9cde042de0b03ce2669fb5949686
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-18 17:34:28 -0400

    Validar los campos

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,14 +1,37 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
 
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mesanje');
+
 
 eventListeners();
 function eventListeners() {
+    // Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 // Funciones
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Valida el formulario
+function validarFormulario(e) {
+    
+    if(e.target.value.length > 0) {
+        console.log('Si hay algo');
+    } else {
+        e.target.style.borderBottomColor = 'red';
+        e.target.classList.add('border', 'border-red-500');
+    }
+
 }
