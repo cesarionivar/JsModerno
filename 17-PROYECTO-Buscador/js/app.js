d3cb0da2e20f0c91b53ad12bd9ac984753d81004
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-20 08:03:04 -0400

    Generar un select de año

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,9 +1,17 @@
 // Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos(); // Muestra los autos al cargar
+
+    // Llena las opciones de años
+    llenarSelect();
 })
 
 // Funciones
@@ -20,4 +28,17 @@ function  mostrarAutos() {
         // Insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+// Genera los años del select
+function llenarSelect() {
+    
+    for (let i = max; i > min; i--) {
+        
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de años al select
+        
+    }
 }
