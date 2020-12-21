209a3e1681f262929b7b29e75e9e26ef1c871af0
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-21 11:33:23 -0400

    Validando el formulario del tweet

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -16,6 +16,35 @@ function eventListeners() {
 // Funciones
 function agregarTweet(e) {
     e.preventDefault();
+
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación...
     
-    console.log('Argegando tweet...');
+    if(tweet.trim() === '') {
+        mostrarError('Un tweet no puede ir vacío');
+        return; // Evita que se ejecuten más líneas de código
+    } 
+    
+    console.log('Agregando tweet');
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina el mensaje despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+
+
 }
