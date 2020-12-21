bca2bc3566b8af8664acfa970447b3001b2e4858
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-12-21 11:18:46 -0400

    Iniciando proyecto localStorage

---
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,21 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+
+// Event listeners
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+    
+    console.log('Argegando tweet...');
+}
