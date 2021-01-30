f465c2ca8a3f489c05b27547e50158074287bb97
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-30 19:01:12 -0400

    Exportar e Importar Funciones

--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -3,6 +3,17 @@ export const nombreCliente = 'Cesario';
 export const ahorro = 200;
 
 
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+    if(ahorro > 0) {
+        console.log('Si tiene saldo');
+    } else {
+        console.log('El cliente no tiene saldo');
+    }
+}
 
 
 
