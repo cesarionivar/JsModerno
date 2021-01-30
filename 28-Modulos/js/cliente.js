7e25712182e3fde9e25bf017ecdbe7143690ea8b
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-30 18:57:44 -0400

    Basicos de los Modulos en ES6

---
+++ 28-Modulos/js/cliente.js
@@ -0,0 +1,23 @@
+
+export const nombreCliente = 'Cesario';
+export const ahorro = 200;
+
+
+
+
+
+
+
+
+
+
+
+
+// IIFE
+/* (function(){
+
+    console.log('Desde un IIFE');
+    
+    window.nombreCliente = 'Cesario';
+
+})();*/
