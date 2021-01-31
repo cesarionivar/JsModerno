c430b752407113d7a701a3310646f5427238387d
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-30 20:00:29 -0400

    Agregando Modulos al Proyecto de Citas

---
+++ 29-PROYECTO-CitasModulos/js/classes/App.js
@@ -0,0 +1,31 @@
+import { datosCita, nuevaCita } from '../funciones.js';
+
+import {
+    fechaInput, 
+    formulario, 
+    horaInput, 
+    mascotaInput, 
+    propietarioInput, 
+    sintomasInput, 
+    telefonoInput
+} from '../selectores.js';
+
+class App {
+    constructor() {
+        this.initApp();
+    }
+
+    initApp() {
+        mascotaInput.addEventListener('change', datosCita);
+        propietarioInput.addEventListener('change', datosCita);
+        telefonoInput.addEventListener('change', datosCita);
+        fechaInput.addEventListener('change', datosCita);
+        horaInput.addEventListener('change', datosCita);
+        sintomasInput.addEventListener('change', datosCita);
+    
+    
+        formulario.addEventListener('submit', nuevaCita);
+    }
+}
+
+export default App;
