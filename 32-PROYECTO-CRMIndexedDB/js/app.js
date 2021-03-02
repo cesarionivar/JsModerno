db22dbf1fe108956cd7d62ac1de306784352e7b4
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-02 17:55:49 -0400

    Eliminar clientes de la BBDD

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,6 +1,7 @@
 (function() {
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', ()=> {
 
@@ -10,7 +11,34 @@
             obtenerCliente();
         }
 
+        listadoClientes.addEventListener('click', eliminarRegistro);
+
     });
+
+    function eliminarRegistro(e) {
+        if(e.target.classList.contains('eliminar')) {
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas eliminar este cliente?');
+
+            if(confirmar) {
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function() {
+                    console.log('Eliminado...');
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function() {
+                    console.log('Hubo un error');
+                }
+            }
+
+        }
+    }
 
     // Create IndexedDB database
     function crearDB() {
@@ -62,7 +90,7 @@
 
                     const { nombre, empresa, email, telefono, id } = cursor.value;
 
-                    const listadoClientes = document.querySelector('#listado-clientes');
+                    
                     listadoClientes.innerHTML += ` 
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -77,7 +105,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 eliminar hover:text-red-900">Eliminar</a>
                             </td>
                         </tr> 
                     `;
