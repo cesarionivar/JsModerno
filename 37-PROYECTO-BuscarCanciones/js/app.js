43eec8e23880d41ea0ac8c6b1cf10441dca8d5d6
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-07 23:58:35 -0400

    Validar campo proyecto letras de cancion

--- 37-PROYECTO-BuscarCanciones/js/app.js
+++ 37-PROYECTO-BuscarCanciones/js/app.js
@@ -4,4 +4,19 @@ UI.formularioBuscar.addEventListener('submit', buscarCancion);
 
 function buscarCancion(e) {
     e.preventDefault();
+
+    // Obtener datos del formulario 
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if(artista === '' || cancion === '') {
+        // El usuario dejo al menos un campo vacio, mostrar error
+        UI.divMensajes.textContent = 'Error... todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMensajes.textContent = '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+    }
 }
