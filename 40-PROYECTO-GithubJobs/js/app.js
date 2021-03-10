845bb02b878d0385dae797704e46a4722baa12d6
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-10 16:08:45 -0400

    Github Jobs API - Validar formulario

---
+++ 40-PROYECTO-GithubJobs/js/app.js
@@ -0,0 +1,42 @@
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    formulario.addEventListener('submit', validarBusqueda);
+
+});
+
+function validarBusqueda(e) {
+    e.preventDefault();
+
+    const busqueda = document.querySelector('#busqueda').value;
+
+    if(busqueda.length < 2) {
+        mostrarMensaje('Búsqueda muy corta... Añade más información');
+        return;
+    }
+
+    consultarAPI();
+}
+
+function consultarAPI() {
+    
+}
+
+function mostrarMensaje(mensaje) {
+    
+    const alertaPrevia = document.querySelector('.alerta');
+
+    if(!alertaPrevia) {     
+        const alerta = document.createElement('div');
+        alerta.classList.add('bg-gray-100', 'p-3', 'text-center', 'mt-3', 'alerta');
+        alerta.textContent = mensaje;
+        
+        formulario.appendChild(alerta);
+        
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
