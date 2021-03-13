354b2ee86947dd713f0c63ae752cea08ed01ff0a
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-13 10:59:29 -0400

    Pure Functions

---
+++ 45-FunctionalJS/js/06-app.js
@@ -0,0 +1,11 @@
+// Funciones puras o Pure Functions
+
+const numeroDuplicar = 20;
+const duplicar = numero => numero * 2;
+
+const resultado = duplicar(numeroDuplicar);
+
+console.log(resultado);
+console.log(numeroDuplicar);
+
+
