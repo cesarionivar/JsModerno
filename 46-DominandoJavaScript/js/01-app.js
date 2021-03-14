4c926be36bbca24269cae2df263365f099a0aef3
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-14 09:32:23 -0400

    Dominando JavaScript - Scope

---
+++ 46-DominandoJavaScript/js/01-app.js
@@ -0,0 +1,29 @@
+/* const login = true;
+
+function mostrarCliente() {
+    const cliente = 'Cesario';
+    console.log(cliente);
+    
+    if(login) {
+        const cliente = 'Admin';
+        console.log(cliente);
+    }
+}
+
+
+mostrarCliente(); */
+
+
+const login = true;
+const cliente = 'Cesario';
+
+function clienteLogueado() {
+    console.log(cliente);
+}
+
+function funcion() {
+    console.log(cliente);
+}
+
+funcion();
+clienteLogueado();
