ec46683e1e2d55ba462f6f975f45c7ef1c4bbbad
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-14 10:15:22 -0400

    Dominando JavaScript - Event Loop

---
+++ 46-DominandoJavaScript/js/07-app.js
@@ -0,0 +1,26 @@
+// Event Loop o Loop de eventos js
+
+console.log('Primero');
+
+
+setTimeout(() => {
+    console.log('Segundo');
+}, 0);
+
+console.log('Tercero');
+
+setTimeout(() => {
+    console.log('Cuarto');
+}, 0);
+
+new Promise(function(resolve) {
+    resolve('Desconocido...')
+}).then(console.log);
+
+console.log('Ultimo');
+
+function hola(){
+    console.log('Hola');
+}
+
+hola();
