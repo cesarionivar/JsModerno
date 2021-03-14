986ce6b28a74a68cb62b3307318effbd33281978
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-14 10:19:59 -0400

    Dominando JavaScript - Self

---
+++ 46-DominandoJavaScript/js/08-app.js
@@ -0,0 +1,16 @@
+
+self.onload = () => {
+    console.log('Ventana lista');
+}
+
+window.nombre = 'Monitor 20 Pulgadas';
+
+const producto = { 
+    precio: 30,
+    disponible: true,
+    mostrarInfo: function() {
+        return `El Producto: ${self.nombre}`
+    }
+}
+
+console.log(producto.mostrarInfo());
