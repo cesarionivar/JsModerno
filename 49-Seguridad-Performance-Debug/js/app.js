d4fdc5c91507c4f44e9024c114256d97ed9bb573
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-17 17:11:39 -0400

    Debugger

--- 49-Seguridad-Performance-Debug/js/app.js
+++ 49-Seguridad-Performance-Debug/js/app.js
@@ -122,33 +122,32 @@ function consultarAPI() {
 }
 
 function mostrarCotizacionHTML(cotizacion) {
-
+    
     limpiarHTML();
-
+    
     console.log(cotizacion);
     const  { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
-
-
-    debugger;
+    
+    // debugger;
 
     const precio = document.createElement('p');
     precio.classList.add('precio');
     precio.innerHTML = `El Precio es: <span> ${PRICE} </span>`;
-
+    
     const precioAlto = document.createElement('p');
     precioAlto.innerHTML = `<p>Precio más alto del día: <span>${HIGHDAY}</span> </p>`;
-
+    
     const precioBajo = document.createElement('p');
     precioBajo.innerHTML = `<p>Precio más bajo del día: <span>${LOWDAY}</span> </p>`;
-
+    
     const ultimasHoras = document.createElement('p');
     ultimasHoras.innerHTML = `<p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span></p>`;
-
+    
     const ultimaActualizacion = document.createElement('p');
     ultimaActualizacion.innerHTML = `<p>Última Actualización: <span>${LASTUPDATE}</span></p>`;
-
+    
     debugger;
-
+    
     resultado.appendChild(precio);
     resultado.appendChild(precioAlto);
     resultado.appendChild(precioBajo);
