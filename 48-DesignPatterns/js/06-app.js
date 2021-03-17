59056e3417dda59a2862ce4c54bed1662b53cf18
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-16 22:55:40 -0400

    Mixin Pattern

--- 48-DesignPatterns/js/06-app.js
+++ 48-DesignPatterns/js/06-app.js
@@ -1,3 +1,39 @@
+class Persona {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
 
-console.log(modulo1.nombre);
-modulo1.hola();
+class Cliente extends Persona {
+
+    constructor(nombre, email, empresa) {
+        super(nombre, email);
+        this.empresa = empresa;
+    }
+}
+
+const funcionesPersona = {
+    mostrarInformacion() {
+        console.log(`Nombre Persona: ${this.nombre} Email: ${this.email}`);
+    },
+    mostrarNombre() {
+        console.log(`Mi nombre es: ${this.nombre}`);
+    }
+}
+
+// Añadir funcionesPersona a la clase de Persona
+Object.assign(Persona.prototype, funcionesPersona);
+Object.assign(Cliente.prototype, funcionesPersona);
+
+
+const cliente = new Persona('Cesario', 'hola@cesarionivar.com');
+
+console.log(cliente);
+cliente.mostrarInformacion();
+cliente.mostrarNombre();
+
+const cliente2 = new Cliente('Cliente', 'cliente@cliente.com');
+console.log(cliente2);
+cliente2.mostrarInformacion();
+cliente2.mostrarNombre();
