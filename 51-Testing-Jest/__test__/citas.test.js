de1c280df2a01df27fca17dea980953c311f81e6
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-20 13:56:31 -0400

    Testing citas - Actualizar y Eliminar

--- 51-Testing-Jest/__test__/citas.test.js
+++ 51-Testing-Jest/__test__/citas.test.js
@@ -3,10 +3,12 @@ import Citas from '../js/classes/Citas.js';
 describe('Probar la clase de citas', ()=> {
 
     const citas = new Citas();
-
+    const id = Date.now();
+    
     test('Agregar una nueva cita', ()=> {
-
+        
         const citaObj = {
+            id,
             mascota: 'Hook',
             propietario: 'Cesario',
             telefono: '80945264120',
@@ -15,12 +17,36 @@ describe('Probar la clase de citas', ()=> {
             sintomas: 'Solo duerme'
         };
 
-        citaObj.id = Date.now();
-
         citas.agregarCita(citaObj);
 
         // Prueba
         expect(citas).toMatchSnapshot();
 
     });
+
+    test('actualiazr cita', () => {
+
+        const citaActualizada = {
+            id,
+            mascota: 'Redux',
+            propietario: 'Cesario',
+            telefono: '80945264120',
+            fecha: '20-03-2021',
+            hora: '01:28',
+            sintomas: 'Solo duerme'
+        };
+
+        citas.editarCita(citaActualizada);
+
+        expect(citas).toMatchSnapshot();
+
+
+    });
+
+    test('Elliminar cita', () => {
+        citas.eliminarCita(id);
+
+        expect(citas).toMatchSnapshot();
+    });
+
 });
