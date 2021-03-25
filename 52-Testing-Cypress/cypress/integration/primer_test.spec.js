01febd30509d3cdcb023d75a1cfa57d243cb4d1f
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-25 17:31:37 -0400

    Atributos en Cypress

--- 52-Testing-Cypress/cypress/integration/primer_test.spec.js
+++ 52-Testing-Cypress/cypress/integration/primer_test.spec.js
@@ -7,9 +7,25 @@ describe('Carga la página prinicpal', () => {
         cy.visit('http://127.0.0.1:5500/52-Testing-Cypress/index.html');
                 
         cy.contains('h1', 'Administrador de Pacientes de Veterinaria');
+        
+        // cy.get('h1').should('exist');
 
-        cy.get('h1').should('exist');
 
+        cy.contains('[data-cy="titulo-proyecto"]', 'Administrador de Pacientes de Veterinaria');
+        
+        cy.get('[data-cy="titulo-proyecto"]').should('exist');
+
+
+        // Verificar que exista el elemento y contenga un texto
+        cy.get('[data-cy="titulo-proyecto"]')
+            .invoke('text')
+            .should('equal', 'Administrador de Pacientes de Veterinaria');
+
+        
+        // Verificar el texto de las citas
+        cy.get('[data-cite="citas-heading"]')
+            .invoke('text')
+            .should('equal', 'No hay Citas, comienza creando una');
     });
 
 });
