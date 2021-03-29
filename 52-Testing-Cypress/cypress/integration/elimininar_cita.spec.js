28cf6e1449de2bfb7a8eabdcc1b0cc85cafeacc2
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-29 16:57:52 -0400

    Test eliminar cita cypress

---
+++ 52-Testing-Cypress/cypress/integration/elimininar_cita.spec.js
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+
+describe('Llena los campos para una nueva cita y la elimina', () => {
+
+    it('Campos nueva cita', () => {
+
+        cy.visit('/index.html');
+        
+        cy.get('[data-cy="mascota-input"]')
+            .type('Hook');
+
+        cy.get('[data-cy="propietario-input"]')
+            .type('Cesario');
+
+        cy.get('[data-cy="telefono-input"]')
+            .type('8093465343');
+
+        cy.get('[data-cy="fecha-input"]')
+            .type('2021-03-29');
+        
+        cy.get('[data-cy="hora-input"]')
+            .type('16:29');
+        
+        cy.get('[data-cy="sintomas-textarea"]')
+            .type('Solo duerme!!');
+        
+        cy.get('[data-cy="submit"]')
+            .click();
+
+        cy.get('[data-cy="citas-heading"]')
+            .invoke('text')
+            .should('equal', 'Administra tus Citas');
+
+        cy.get('[data-cy="alerta"]')
+        .invoke('text')
+        .should('equal', 'Se agregó correctamente');
+        
+        cy.get('[data-cy="alerta"]')
+            .should('have.class', 'alert-success');
+        
+
+    });
+
+    it('Eliminar una cita', () => {
+
+        cy.get('[data-cy="btn-eliminar"]')
+            .click();
+
+        cy.get('[data-cy="citas-heading"]')
+            .invoke('text')
+            .should('equal', 'No hay Citas, comienza creando una');
+
+
+    });
+
+});
