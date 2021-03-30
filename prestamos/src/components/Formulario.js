630e09746864a2b44dc3e47e828aaa009993f5dd
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-30 17:09:08 -0400

    Mostrar mensaje de error

--- prestamos/src/components/Formulario.js
+++ prestamos/src/components/Formulario.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
 
@@ -14,42 +14,48 @@ const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
       guardarError(true);
     }
 
+    guardarError(false);
+
   }
 
   return (
-    <form onSubmit={calcularPrestamo}>
-      <div className="row">
-        <div>
-          <label>Cantidad Prestamo</label>
-          <input
-            className="u-full-width"
-            type="number"
-            placeholder="Ejemplo: 3000"
-            onChange={ e => guardarCantidad( parseInt(e.target.value) ) }
-          />
+    <Fragment>
+      <form onSubmit={calcularPrestamo}>
+        <div className="row">
+          <div>
+            <label>Cantidad Prestamo</label>
+            <input
+              className="u-full-width"
+              type="number"
+              placeholder="Ejemplo: 3000"
+              onChange={ e => guardarCantidad( parseInt(e.target.value) ) }
+            />
+          </div>
+          <div>
+            <label>Plazo para Pagar</label>
+            <select 
+              className="u-full-width"
+              onChange={e => parseInt(guardarPlazo(e.target.value) )}
+            >
+              <option value="">Seleccionar</option>
+              <option value="3">3 meses</option>
+              <option value="6">6 meses</option>
+              <option value="12">12 meses</option>
+              <option value="24">24 meses</option>
+            </select>
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Calcular"
+              className="button-primary u-full-width"
+            />
+          </div>
         </div>
-        <div>
-          <label>Plazo para Pagar</label>
-          <select 
-            className="u-full-width"
-            onChange={e => parseInt(guardarPlazo(e.target.value) )}
-          >
-            <option value="">Seleccionar</option>
-            <option value="3">3 meses</option>
-            <option value="6">6 meses</option>
-            <option value="12">12 meses</option>
-            <option value="24">24 meses</option>
-          </select>
-        </div>
-        <div>
-          <input
-            type="submit"
-            value="Calcular"
-            className="button-primary u-full-width"
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+      {(error) ? <p className="error">Todos los campos son obligatorios</p> : null }
+      
+    </Fragment>
   );
 };
 
