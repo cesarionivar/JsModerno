479bbe8679fc50d459594033626c73929dea107b
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-04-09 15:20:18 -0400

    Creando las plantillas de la vistas

--- agenciaViajesNode/routes/index.js
+++ agenciaViajesNode/routes/index.js
@@ -3,12 +3,30 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
-  res.render('inicio');
+  res.render('inicio', {
+    pagina: 'Inicio'
+  });
 });
+
 router.get('/nosotros', (req, res) => { 
 
+  res.render('nosotros', {
+    pagina: 'Nosotros'
+  });
+});
 
-  res.render('nosotros');
+router.get('/viajes', (req, res) => { 
+
+  res.render('viajes', {
+    pagina: 'Viajes'
+  });
+});
+
+router.get('/testimoniales', (req, res) => { 
+
+  res.render('testimoniales', {
+    pagina: 'Testimoniales'
+  });
 });
 
 export default router;
