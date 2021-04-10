07dccdc99f8dc8ae0b417103d62df2e050539b5f
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-04-10 13:24:18 -0400

    MVC - Model Viaje with Sequelize

---
+++ agenciaViajesNode/models/Viaje.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize';
+import db from '../config/db.js';
+
+export const Viaje = db.define('viajes', {
+  
+  titulo: {
+    type: Sequelize.STRING
+  },
+  precio: {
+    type: Sequelize.STRING
+  },
+  fecha_ida: {
+    type: Sequelize.DATE
+  },
+  fecha_vuelta: {
+    type: Sequelize.STRING
+  },
+  imagen: {
+    type: Sequelize.STRING
+  },
+  descripcion: {
+    type: Sequelize.STRING
+  },
+  slug: {
+    type: Sequelize.STRING
+  }
+
+});
